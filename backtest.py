29169136d594ff256a77b7a6181df169e2bc3a37
Brian Lim <limbc122@gmail.com>
2018-09-07 17:39:58 +0900

    correct syntax error

--- backtest.py
+++ backtest.py
@@ -2,14 +2,18 @@ import pandas as pd
 import numpy as np
 
 class Backtest:
-    def __init__(self, candle, analysis):
-        assert len(candle) == len(analysis), "Each length of candle data and analysis data are different."
-        self.candle = candle
-        self.analysis = analysis
+    def __init__(self, data):
+        self.data = data
 
     def run(self):
-        for idx, current in self.candle.iterrows():
-            algo(idx)
+        for idx in range(len(self.data)):
+            self.algo(self.data.iloc[:idx])
     
-    def alog(self, idx):
+    def algo(self, data):
+        # Stochastic Cross strategy
+        # Premise : 
+        # Entering spot : sto_k under low limit(25) touched before and currently rising and cross the sto_d
+        # stop loss : sto_k cross the sto_d down again and value is under low limit(25), sto_k is falling during n tick
+        # Exiting spot : 1. rising is ended middle of range
+        #                2. sto_k upper high limit(80) touched and sto_k cross the sto_d down again
         pass
